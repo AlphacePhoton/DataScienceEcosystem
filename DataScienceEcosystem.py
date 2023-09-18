88e809be954d891705e28f64ecd90fc35b8a857f
AlphacePhoton <145264535+AlphacePhoton@users.noreply.github.com>
2023-09-18 18:16:02 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystems are summarized.
+
+# ## Objectives:
+# - List popular languages for Data Science
+# - List commonly used libraries for Data Science
+# - Table of Open-Source Development Environment for Data Science
+# - Evaluating Airthmetic Expressions
+# 
+# 
+
+# #### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. Scala
+# 6. C++
+# 7. Ruby
+# 
+
+# #### Some of the commonly used libraries by Data Scientists are: 
+# (python)
+# 1. Scientific Computing Libraries- Numpy, Pandas
+# - Pandas
+# - Numpy
+# 2. Visualization Libraries
+# - Matplotlib
+# - Seaborn
+# 3. Machine Learning Libraries
+# - Scikit-learn
+# 4. Deep Learning Libraries
+# - Keras
+# - Tensorflow
+# - ApacheSpark
+# - Pytorch
+# 
+# 
+# (Scala)
+# 1. Statistical Data Visualization library: Vegas
+# 2. Deep Learning library: bigDL
+#     
+# (R)
+# 1. Data Visualization Library: ggplot2
+
+# #### Table of Development Environment Open Source Tools Used in Data Science
+# |Data Science Tools|
+# |------------------|
+# |Jupyter|
+# |RStudio|
+# |Apache Spark|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[11]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[12]:
+
+
+# This will convert 200 minutes to hours by divding by 60
+200/60
+
+
+# ## Author
+# Anubhooti Soni
+
+# In[ ]:
+
+
+
+
